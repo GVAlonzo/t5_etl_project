b26af130a6ee75d92a5cb2574655838f1b614640
GVAlonzo <George.V.Alonzo@gmail.com>
2021-11-17 22:17:16 -0600

    Updated SQL

--- sqltables.sql
+++ sqltables.sql
@@ -1,28 +1,29 @@
-DROP TABLE biketrip;
+DROP TABLE bike_trip;
 DROP TABLE station;
 
 -- Create tables for raw data to be loaded into
-
-
-CREATE TABLE biketrip (
-
-ride_id TEXT PRIMARY KEY,
-start_station_id INT,
-FOREIGN KEY (start_station_id) REFERENCES station (station_id),
-start_date TEXT,
-start_time TEXT,
-end_station_id INT,
-FOREIGN KEY (start_station_id) REFERENCES station (station_id),
-end_date TEXT,
-end_time TEXT,  
-duration FLOAT
-membership_type TEXT,
-city_name TEXT,
-);
-
 CREATE TABLE station (
-station_id INT PRIMARY KEY,
+station_id TEXT PRIMARY KEY,
 lattitude FLOAT,
 longitude FLOAT);
 
-SELECT * FROM BIKETRIP;
+CREATE TABLE bike_trip (
+
+ride_id TEXT PRIMARY KEY,
+start_station_id TEXT,
+FOREIGN KEY (start_station_id) REFERENCES station (station_id),
+start_date TEXT,
+start_time TEXT,
+end_station_id TEXT,
+FOREIGN KEY (start_station_id) REFERENCES station (station_id),
+end_date TEXT,
+end_time TEXT,  
+duration FLOAT,
+membership_type TEXT,
+city_name TEXT
+);
+
+
+
+SELECT * FROM station;
+SELECT * FROM bike_trip;
